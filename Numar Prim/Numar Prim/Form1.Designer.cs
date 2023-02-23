894c8a2102dfacad7fd02c732a5e7e2773aaff2e
unknown <vladpc87@gmail.com>
2023-02-23 21:35:52 +0200

    Determinarea primalitatii unui numar

---
+++ Numar Prim/Numar Prim/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.submitBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(321, 128);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(120, 22);
+            this.numberTextBox.TabIndex = 0;
+            // 
+            // submitBTN
+            // 
+            this.submitBTN.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.submitBTN.Location = new System.Drawing.Point(331, 191);
+            this.submitBTN.Name = "submitBTN";
+            this.submitBTN.Size = new System.Drawing.Size(100, 46);
+            this.submitBTN.TabIndex = 1;
+            this.submitBTN.Text = "Submit";
+            this.submitBTN.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.submitBTN);
+            this.Controls.Add(this.numberTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button submitBTN;
+    }
+}
+
